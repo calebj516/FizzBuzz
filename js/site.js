3b8a33af9dea497a2a8875082e16a5bbb60ce524
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-02-09 21:02:28 -0600

    FizzBuzz: App Page & JS
    1. Added an event listener on the submit button on app.html that calls the getValues function in site.js
    2. Completed getValues and calculateResults functions.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,52 @@
-// call Hello World
-function HelloWorld() {
-  alert("Hello World!");
+
+// get values from the page
+function getValues()  {
+  const startVal = document.getElementById('startValue').value;
+  const endVal = document.getElementById('endValue').value;
+
+  // call calculateResults
+  const results = calculateResults(startVal, endVal);
+  // call display results
+  displayResults(results);
 }
+
+// calculate results
+function calculateResults(startVal, endVal) {
+  const nums = [];
+
+  for(let i = startVal; i <= endVal; i++){
+
+    if(i % 5 === 0 && i % 3 === 0){
+      nums.push("FizzBuzz");
+    } else if(i % 5 === 0){
+      nums.push("Buzz");
+    } else if(i % 3 === 0){
+      nums.push("Fizz");
+    } else {
+      nums.push(i);
+    }
+
+  }
+
+  return nums;
+}
+
+// display results
+function displayResults(nums) {
+
+  let templateRow = "";
+  let templateData = "";
+
+  for(let i = 0; i < nums.length; i++){
+
+    let number = nums[i];
+    templateData += `<td>${number}</td>`;
+    // templateRow += `<tr>${templateData}</tr>`;
+  }
+
+
+
+  document.getElementById('results').innerHTML = templateData;
+
+}
+
