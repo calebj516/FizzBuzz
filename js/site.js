dcb7140c3f0bbe7dd1f021fd7f0a690bab8560d3
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-02-14 21:49:24 -0600

    FizzBuzz: App Page Continued Part 2
    1. Modified layout of app.html
    2. Modified JS in site.js
    3. Modified CSS in site.css to style certain numbers in the results

--- js/site.js
+++ js/site.js
@@ -1,57 +1,84 @@
 
-// get values from the page
 function getValues()  {
-  const startVal = document.getElementById('startValue').value;
-  const endVal = document.getElementById('endValue').value;
+  // get values from the page
+  let fizzVal = document.getElementById('fizz').value;
+  let buzzVal = document.getElementById('buzz').value;
 
   // call calculateResults
-  const results = calculateResults(startVal, endVal);
+  const fizzBuzzArray = calculateResults(fizzVal, buzzVal);
   // call display results
-  displayResults(results);
+  displayResults(fizzBuzzArray);
+  
 }
 
 // calculate results
-function calculateResults(startVal, endVal) {
+function calculateResults(fizzVal, buzzVal) {
+
+  // initialize return array
   const nums = [];
 
-  for(let i = startVal; i < endVal; i++){
-
-    if(i % 5 === 0 && i % 3 === 0){
+  // loop from 1 to 100
+  for(let i = 1; i <= 100; i++){
+    // if statement logic:
+    // check to see if divisible by both 3 and 5
+    // check to see if divisible by 5
+    // check to see if divisible by 3
+    // else push number to array
+    if(i % fizzVal === 0 && i % buzzVal === 0){
       nums.push("FizzBuzz");
-    } else if(i % 5 === 0){
+    } else if(i % buzzVal === 0){
       nums.push("Buzz");
-    } else if(i % 3 === 0){
+    } else if(i % fizzVal === 0){
       nums.push("Fizz");
     } else {
       nums.push(i);
     }
-
   }
 
+  // return array
   return nums;
 }
 
-// display results
-function displayResults(nums) {
+// loop over array and create a table row for each item
+function displayResults(fbData) {
 
-  let templateRows = "";
-  let templateData = "";
-  let count = 1;
+  // get the table body element from the page
+  const tableBody = document.getElementById('results');
 
-  for(let i = 0; i <= Math.ceil(nums.length / 5); i++){
+  // get the template row
+  const templateRow = document.getElementById('fbTemplate');
 
-    for(let j = 0; j < 5; j++){
-      // let slicedArr = nums.slice(0, 4)
-      templateData += `<td>${nums[j]}</td>`;
-    }
+  // clear table first
+  tableBody.innerHTML = "";
 
-    templateRows += `<tr>${templateData}</tr><br>`;
-    templateData = "";
-    nums = nums.slice(count += 4);
+  for(let i = 0; i < fbData.length; i += 5){
 
+    // import template element from app.html as a document fragment (essentially make a copy of it)
+    // true includes the node's descendants
+    let tableRow = document.importNode(templateRow.content, true);
+    // grab the td elements to put into an array
+    let rowCols = tableRow.querySelectorAll('td');
+    
+    // set the class to the element in fbData 
+    // if it is "Fizz", "Buzz", or "FizzBuzz", certain CSS rules to change the color and font-weight will be triggered.
+    rowCols[0].classList.add(fbData[i]);
+    rowCols[0].textContent = fbData[i];
+
+    rowCols[1].classList.add(fbData[i + 1]);
+    rowCols[1].textContent = fbData[i + 1];
+
+    rowCols[2].classList.add(fbData[i + 2]);
+    rowCols[2].textContent = fbData[i + 2];
+
+    rowCols[3].classList.add(fbData[i + 3]);
+    rowCols[3].textContent = fbData[i + 3];
+
+    rowCols[4].classList.add(fbData[i + 4]);
+    rowCols[4].textContent = fbData[i + 4];
+
+    // add all the rows to the table
+    tableBody.appendChild(tableRow);
   }
-
-  document.getElementById('results').innerHTML = templateRows;
 
 }
 
